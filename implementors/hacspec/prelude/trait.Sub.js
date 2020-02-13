5faf417bb6036484aa10fc5087b820933eef4932
franziskuskiefer <franziskuskiefer@users.noreply.github.com>
2020-02-13 19:06:04 +0000

    deploy: e0f818b5cf560ef2fbefa073c425a99c5f88a0cc

--- implementors/hacspec/prelude/trait.Sub.js
+++ implementors/hacspec/prelude/trait.Sub.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["hacspec"] = [];
+implementors["hacspec"] = [{"text":"impl&lt;T:&nbsp;<a class=\"trait\" href=\"hacspec/poly/trait.TRestrictions.html\" title=\"trait hacspec::poly::TRestrictions\">TRestrictions</a>&lt;T&gt;&gt; <a class=\"trait\" href=\"hacspec/prelude/trait.Sub.html\" title=\"trait hacspec::prelude::Sub\">Sub</a>&lt;<a class=\"struct\" href=\"hacspec/poly/struct.Poly.html\" title=\"struct hacspec::poly::Poly\">Poly</a>&lt;T&gt;&gt; for <a class=\"struct\" href=\"hacspec/poly/struct.Poly.html\" title=\"struct hacspec::poly::Poly\">Poly</a>&lt;T&gt;","synthetic":false,"types":["hacspec::poly::Poly"]}];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
